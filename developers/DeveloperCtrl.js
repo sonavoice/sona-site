33913fc5bbc35be14343022f72091a95353c3b72
Garrett Maring <garrett.maring@gmail.com>
2015-11-03 15:07:12 -0800

    Angular routes and structure

---
+++ developers/DeveloperCtrl.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+  .controller('DeveloperCtrl', DeveloperCtrl);
+
+  function DeveloperCtrl() {
+    var self = this;
+
+    
+  }
+
+})();
---
+++ extensions/ExtensionCtrl.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+  .controller('ExtensionsCtrl', ExtensionsCtrl);
+
+  function ExtensionsCtrl() {
+    var self = this;
+
+    
+  }
+
+})();
